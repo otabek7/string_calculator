7bad38bbae834f5a0d527246e265dd443b101fe1
Otabek Mavlonov <mavlonovo@lenovolaptop.localdomain>
2022-09-28 20:27:18 -0500

    string_calculator first commit

---
+++ string_calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "./string_calculator.h"
+
+using std::cout, std::endl;
+using std::string;
+
+unsigned int digit_to_decimal(char digit) {
+    // TODO(student): implement
+    return 0;
+}
+
+char decimal_to_digit(unsigned int decimal) {
+    // TODO(student): implement
+    return '\0';
+}
+
+string trim_leading_zeros(string num) {
+    // TODO(student): implement
+    return "";
+}
+
+string add(string lhs, string rhs) {
+    // TODO(student): implement
+    return "";
+}
+
+string multiply(string lhs, string rhs) {
+    // TODO(student): implement
+    return "";
+}
